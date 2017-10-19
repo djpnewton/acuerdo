3132901ce4dfbaf19cd13df738ea68695fce7132
Daniel Newton <djpnewton@gmail.com>
2017-10-19 21:04:10 +1300

    add logging to file

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using viafront3.Data;
 using viafront3.Models;
 using viafront3.Services;
@@ -40,7 +41,7 @@ namespace viafront3
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -62,6 +63,8 @@ namespace viafront3
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            loggerFactory.AddFile("logs/viafront-{Date}.txt");
         }
     }
 }
