2fa72ae26458a3d53b1ac6b5538c952f5719ea99
Daniel Newton <djpnewton@gmail.com>
2017-10-24 18:31:04 +1300

    add via_jsonrpc lib; show basic order book

---
+++ Controllers/MarketController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using viafront3.Models;
+using via_jsonrpc;
+
+namespace viafront3.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class MarketController : Controller
+    {
+        public IActionResult Orderbook()
+        {
+            var via = new ViaJsonRpc("http://10.50.1.2:8080");
+            var orderDepth = via.OrderDepthQuery("BTCCNY", 10, "1");
+
+            ViewData["Market"] = "BTC/CNY";
+            ViewData["AmountUnit"] = "BTC";
+            ViewData["PriceUnit"] = "CNY";
+            ViewData["OrderDepth"] = orderDepth;
+
+            return View();
+        }
+    }
+}
