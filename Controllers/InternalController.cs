5001d4777a647ddf4f79aa865e78dbbf990ff695
Daniel Newton <djpnewton@gmail.com>
2017-12-11 15:15:56 +1300

    add users page

--- Controllers/InternalController.cs
+++ Controllers/InternalController.cs
@@ -36,6 +36,25 @@ namespace viafront3.Controllers
             return View(BaseViewModel());
         }
 
+        public IActionResult Users()
+        {
+            var user = GetUser(required: true).Result;
+
+            var users = (from u in _context.Users
+                let query = (from ur in _context.Set<IdentityUserRole<string>>()
+                    where ur.UserId.Equals(u.Id)
+                    join r in _context.Roles on ur.RoleId equals r.Id select r.Name)
+                    select new UserRoleInfo() {User = u, Roles = query.ToList<string>()})
+                    .ToList();
+
+            var model = new UsersViewModel
+            {
+                User = user,
+                UserRoleInfos = users
+            };
+            return View(model);
+        }
+
         [AllowAnonymous]
         [Produces("application/json")]
         public IActionResult WebsocketAuth()
---
+++ Models/InternalViewModels/UsersViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using via_jsonrpc;
+
+namespace viafront3.Models.InternalViewModels
+{
+    public class UserRoleInfo
+    {
+        public ApplicationUser User { set; get; }
+        public List<string> Roles { set; get; } 
+    }
+
+    public class UsersViewModel : BaseViewModel
+    {
+        public List<UserRoleInfo> UserRoleInfos { get; set; }
+    }
+}
