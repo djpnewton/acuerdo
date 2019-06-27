d8b01e92fd9e1b0266a1b950bf91b2fb55c89812
Daniel Newton <djpnewton@gmail.com>
2019-06-26 19:47:20 -0500

    fix internals forms with strict CSP

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,21 @@
-﻿// Write your JavaScript code.
+﻿function ready(fn) {
+    if (document.readyState != 'loading'){
+        fn();
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', fn);
+    } else {
+        document.attachEvent('onreadystatechange', function() {
+        if (document.readyState != 'loading')
+            fn();
+        });
+    }
+}
+
+ready(function() {
+    // submit the form when element changes
+    $(".onchange-submit").each(function(index) {
+        $(this).change(function() {
+            this.form.submit();
+        });
+    });
+});
