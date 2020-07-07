dc5f170a46b86ee9300d97c12e8449b06a9caf56
Daniel Newton <djpnewton@gmail.com>
2020-07-07 18:45:00 +1200

    add ef migration for ef core 3 and BrokerOrder.User

--- Data/Migrations/20200707063455_EfCore3_BrokerOrderDotUser.Designer.cs
+++ Data/Migrations/20200707063455_EfCore3_BrokerOrderDotUser.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using viafront3.Data;
 
 namespace viafront3.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200707063455_EfCore3_BrokerOrderDotUser")]
+    partial class EfCore3_BrokerOrderDotUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
